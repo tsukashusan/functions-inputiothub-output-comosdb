e86ec11cf45de079324b7152f91b07bb5af75be1
tsukabon <shtsukam@outlook.com>
2023-02-07 00:00:58 +0900

    update sensorcopy1

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 app = func.FunctionApp()
 
 @app.function_name(name="EventHubTrigger1")
-@app.cosmos_db_output(arg_name="doc", database_name="iotdb", collection_name="sensorcopy", connection_string_setting="connection_string_setting", create_if_not_exists=True, partition_key="/deviceid")
+@app.cosmos_db_output(arg_name="doc", database_name="iotdb", collection_name="sensorcopy1", connection_string_setting="connection_string_setting", create_if_not_exists=True, partition_key="/deviceid")
 @app.event_hub_message_trigger(arg_name="myhub", event_hub_name="",connection="connectionString")
 def test_function(myhub: func.EventHubEvent,  doc: func.Out[func.Document]):
     logging.info('Python EventHub trigger processed an event: %s', myhub.get_body().decode('utf-8'))
