9a73122c235a791653d936d30e5fa28d4b16ea1b
tsukabon <shtsukam@outlook.com>
2023-02-06 23:51:42 +0900

    update .

--- function_app.py
+++ function_app.py
@@ -1,12 +1,12 @@
 import logging
 import azure.functions as func
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 app = func.FunctionApp()
 
 @app.function_name(name="EventHubTrigger1")
-@app.cosmos_db_output(arg_name="doc", database_name="iotdb", collection_name="sensorcopy", connection_string_setting="connection_string_setting", create_if_not_exists=False)
+@app.cosmos_db_output(arg_name="doc", database_name="iotdb", collection_name="sensorcopy", connection_string_setting="connection_string_setting", create_if_not_exists=True, partition_key="/deviceid")
 @app.event_hub_message_trigger(arg_name="myhub", event_hub_name="",connection="connectionString")
 def test_function(myhub: func.EventHubEvent,  doc: func.Out[func.Document]):
     logging.info('Python EventHub trigger processed an event: %s',
