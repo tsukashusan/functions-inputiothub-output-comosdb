2ee62e01a5bc040fe0bcdd3fcd4745396311c992
tsukabon <shtsukam@outlook.com>
2023-02-06 09:43:20 +0900

    update create_if_not_exists.

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 @app.function_name(name="EventHubTrigger1")
 @app.cosmos_db_output(arg_name="doc", database_name="iotdb", collection_name="sensorcopy", connection_string_setting="connection_string_setting", create_if_not_exists=False)
-@app.event_hub_message_trigger(arg_name="myhub", event_hub_name="iothubjpwest-sample",connection="connectionString")
+@app.event_hub_message_trigger(arg_name="myhub", event_hub_name="iothubjpwest-sample",connection="connection")
 def test_function(myhub: func.EventHubEvent,  doc: func.Out[func.Document]):
     logging.info('Python EventHub trigger processed an event: %s',
                 myhub.get_body().decode('utf-8'))
